13a9a199be5ed956e821f6c8c66eddb85a91689d
CMonkey <42.codemonkey@gmail.com>
2021-11-10 10:02:56 +0800

    init project

---
+++ src/main/java/org/excavator/boot/hibernaterecavator/HibernateRecavatorApplication.java
@@ -0,0 +1,13 @@
+package org.excavator.boot.hibernaterecavator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateRecavatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateRecavatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/excavator/boot/hibernaterecavator/HibernateRecavatorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.excavator.boot.hibernaterecavator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateRecavatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
