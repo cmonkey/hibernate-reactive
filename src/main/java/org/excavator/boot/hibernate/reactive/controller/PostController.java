ec20a5f4d0d69c3eddfe394a729fa73815b5676d
CMonkey <42.codemonkey@gmail.com>
2021-11-10 11:39:37 +0800

    add methods

--- src/main/java/org/excavator/boot/hibernate/reactive/controller/PostController.java
+++ src/main/java/org/excavator/boot/hibernate/reactive/controller/PostController.java
@@ -1,2 +1,27 @@
-package org.excavator.boot.hibernate.reactive.controller;public class PostController {
+package org.excavator.boot.hibernate.reactive.controller;
+
+import org.excavator.boot.hibernate.reactive.entity.Post;
+import org.excavator.boot.hibernate.reactive.repository.PostRepository;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+import javax.annotation.Resource;
+
+@RestController
+@RequestMapping("/posts/v1")
+public class PostController {
+    @Resource
+    PostRepository postRepository;
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from spring boot and hibernate reactive! " + postRepository.findAll().toString();
+    }
+
+    @RequestMapping("/save")
+    public Mono<Post> save(@RequestBody Post post){
+        postRepository.save(post);
+        return Mono.just(postRepository.findById(post.getId()));
+    }
 }
--- src/main/java/org/excavator/boot/hibernate/reactive/repository/PostRepository.java
+++ src/main/java/org/excavator/boot/hibernate/reactive/repository/PostRepository.java
@@ -1,2 +1,30 @@
-package org.excavator.boot.hibernate.reactive.repository;public class PostRepository {
+package org.excavator.boot.hibernate.reactive.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.excavator.boot.hibernate.reactive.entity.Post;
+import org.hibernate.SessionFactory;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component
+@RequiredArgsConstructor
+public class PostRepository {
+
+    private final SessionFactory sessionFactory;
+
+    public java.util.List<Post> findAll(){
+        var query = sessionFactory.getCriteriaBuilder().createQuery(Post.class);
+        var root = query.from(Post.class);
+        return sessionFactory.getCurrentSession().createQuery(query).getResultList();
+    }
+    public Post findById(UUID id){
+        return sessionFactory.getCurrentSession().find(Post.class, id);
+    }
+    public void save(Post post){
+        sessionFactory.getCurrentSession().save(post);
+    }
+    public void delete(Post post){
+        sessionFactory.getCurrentSession().delete(post);
+    }
 }
